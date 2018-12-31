002a7bdbdf99bed235f1bb8a2b878aea5481b12b
Prakash Manandhar <jsaaymi@gmail.com>
2018-12-31 19:40:51 +0000

    adding reference and four symbol sample pictures

--- misc_scripts/TestCamera.py
+++ misc_scripts/TestCamera.py
@@ -5,7 +5,7 @@ camera = PiCamera()
 camera.rotation = -90
 
 camera.start_preview(fullscreen=False, window = (100, 20, 640, 480))
-for i in range(20):
+for i in range(200):
     camera.capture("./imgcap_%03d.jpg"%i)
-    sleep(0.25)
+    sleep(0.1)
 camera.stop_preview()
