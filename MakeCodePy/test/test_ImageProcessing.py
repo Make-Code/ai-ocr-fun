c30a9a1ed051a5a2dcc100a59b5c281043a752f0
Prakash Manandhar <jsaaymi@gmail.com>
2019-01-07 00:12:30 -0500

    starting unittest framework code

---
+++ MakeCodePy/test/test_ImageProcessing.py
@@ -0,0 +1,11 @@
+import unittest
+import MakeCodePy.ImageProcessing as ImProc
+
+class TestImageProcessingFunctions(unittest.TestCase):
+
+    def test_FindRedColor (self):
+        self.assertEqual(True, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
